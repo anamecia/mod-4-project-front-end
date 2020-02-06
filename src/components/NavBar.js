90aa44f79157ebf88453e310b318c31076442dfe
ana <anameciarobalo@gmail.com>
2020-02-06 16:53:02 +0000

    adds a placeholder for app name, sigout link and search form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
 
-const NavBar = () => {
+import SearchForm from './SearchForm'
 
-    return(
-        <div>
-          
-        </div>
-    )
+class  NavBar extends Component {
+
+    state = {
+        displaySearchForm: false
+    }
+
+    toggleSearchForm = () => {
+        this.setState({
+            displaySearchForm: !this.state.displaySearchForm
+        })
+    }
+
+
+    render(){
+        return(
+            <div>
+                <Link to='/'>AppName</Link>
+                <label onClick={this.toggleSearchForm}>Search</label>
+                {this.state.displaySearchForm && <SearchForm />}
+                <Link to='/'>Sig Out </Link>
+            </div>
+        )
+    }
+    
 }
 
 export default NavBar
