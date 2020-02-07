060fc0876a7c17ecb7126438c5adc4193d2e867b
ana <anameciarobalo@gmail.com>
2020-02-07 10:07:40 +0000

    adds routes for SignUpPage and SignInPagein the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 
-import HomePage from './HomePage'
+import HomePage from './HomePage';
 import NavBar from './NavBar';
+import SignUpPage from './SignUpPage';
+import SignInPage from './SignInPage';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <NavBar />
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/signup' component={SignUpPage}/>
+        <Route exact path='/signin' component={SignInPage}/>
       </Switch>
     </div>
   );
