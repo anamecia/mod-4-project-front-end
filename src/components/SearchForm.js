339818da2758a6aab61d55be1e703f7ee4589761
ana <anameciarobalo@gmail.com>
2020-02-07 08:18:40 +0000

    adds a search form, no funcionality yet

---
+++ src/components/SearchForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchForm = () => {
+
+    return(
+        <>
+        <input placeholder='Title/Author/Isbn'/>
+        </>
+    )
+}
+
+export default SearchForm
