aa813f7a0fe9f04f16a219f3bbc4f8b9e1b2d44d
ana <anameciarobalo@gmail.com>
2020-02-07 10:51:13 +0000

    adds new component, MainImage that renders an image

---
+++ src/components/MainImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainImg from '../images/2785837.svg'
+
+const MainImage = () => {
+    
+    return(
+        <img src={MainImg}/>
+    )
+}
+
+export default MainImage
