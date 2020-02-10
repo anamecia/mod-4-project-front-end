5658c4d415bab48e4066d8df05226d786bca6c5f
ana <anameciarobalo@gmail.com>
2020-02-10 13:53:18 +0000

    adds auth to sign in and sign out

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './HomePage';
@@ -6,24 +6,55 @@ import NavBar from './NavBar';
 import SignUpPage from './SignUpPage';
 import SignInPage from './SignInPage';
 import BooksContainer from './BooksContainer';
+import API from './API';
 
-function App() {
-  return (
-    <div className="column ">
-      <div id="navbar" className="ui grid">
-        <NavBar />
+class  App extends Component{
+
+  state = {
+    username: null
+  }
+
+  signIn = (data) => {
+    this.setState({ username: data.username })
+    localStorage.token = data.token
+  }
+
+  signOut = () => {
+    this.setState({ username: null })
+    localStorage.removeItem('token')
+  }
+
+  componentDidMount = () => {
+    if(localStorage.token){
+      API.validate()
+      .then(data => {
+        if (data.error) throw Error(data.error)
+        this.signIn(data)
+       
+      })
+      .catch(error => alert(error))
+    }
+  }
+
+  render(){
+    return (
+      <div className="column ">
+        <div id="navbar" className="ui grid">
+          <NavBar signOut={this.signOut}/>
+        </div>
+        <div className="row content-container">
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/signin' component={props => <SignInPage {...props} signIn={this.signIn}/>}/>
+            <Route exact path='/signup' component={SignUpPage}/>
+            <Route exact path='/books' component={BooksContainer}/>
+            <Route component={props => <h1>404 - Page not found</h1>} />
+          </Switch>
+        </div>
       </div>
-      <div className="row content-container">
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/signup' component={SignUpPage}/>
-          <Route exact path='/signin' component={SignInPage}/>
-          <Route exact path='/books' component={BooksContainer}/>
-          <Route component={props => <h1>404 - Page not found</h1>} />
-        </Switch>
-      </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
