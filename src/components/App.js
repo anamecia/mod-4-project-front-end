37c801d081af306489f57e040870940553d93def
ana <anameciarobalo@gmail.com>
2020-02-11 13:50:07 +0000

    adds a seach feature when user submits his search the searchBookByName method updates the App state seachedBooks and sends the list to Book component

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ import HomePage from './HomePage';
 import NavBar from './NavBar';
 import SignUpPage from './SignUpPage';
 import SignInPage from './SignInPage';
-import BooksContainer from './BooksContainer';
 import API from './API';
 import UserShowPage from './UserShowPage';
 
@@ -14,7 +13,8 @@ class  App extends Component{
 
   state = {
     username: null,
-    searchTerm: null
+    searchTerm: '',
+    searchedBooks: null
   }
 
   signIn = (data) => {
@@ -46,22 +46,30 @@ class  App extends Component{
     })
   }
 
-  // searchByBookName = () => {
-  //   console.log( API.searchBooks(this.state.searchTerm))
-  // }
+  searchByBookName = () => {
+        API.searchBook(this.state.searchTerm)
+      .then(data => data.items.map((book) => {return{
+        title: book.volumeInfo.title,
+        description: book.volumeInfo.description,
+        author:book.volumeInfo.authors,
+        image:book.volumeInfo.imageLinks.thumbnail || null,
+        genre: book.volumeInfo.categories}}))
+        .then(bookList => this.setState({searchedBooks: bookList}))
+    }
+
 
   render(){
     return (
       <div className="column ">
         <div id="navbar" className="ui grid">
-          <NavBar signOut={this.signOut} updateSearchTerm={this.updateSearchTerm}/>
+          <NavBar signOut={this.signOut} updateSearchTerm={this.updateSearchTerm} searchByBookName={this.searchByBookName}/>
         </div>
         <div className="row content-container">
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route exact path='/signin' component={props => <SignInPage {...props} signIn={this.signIn}/>}/>
             <Route exact path='/signup' component={props => <SignUpPage {...props} signIn={this.signIn}/>} />
-            <Route exact path='/userbooks' component={props => <UserShowPage {...props} username={this.state.username}/>}/>
+            <Route exact path='/userbooks' component={props => <UserShowPage {...props} username={this.state.username}  searchTerm={this.state.searchTerm} searchedBooks={this.state.searchedBooks}/>}/>
             <Route component={props => <h1>404 - Page not found</h1>} />
           </Switch>
         </div>
--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
 
-import API from './API'
 import Book from './Book'
 
-class BooksContainer extends Component {
+const BooksContainer = ({books, searchedBooks}) => {
 
-
-    render(){
-        return (
-            <div className="ui divided three column grid">
-                {/* {this.props.books.map(book => <Book book={book} key={book.id}/>)} */}
-            </div>
-        )
-    }
+    return (
+        <div className="ui divided three column grid">
+            {searchedBooks
+                ?searchedBooks.map(book => <Book book={book} key={book.id}/>)
+                :books.map(book => <Book book={book} key={book.id}/>)}
+        </div>
+    )
 }
 
 export default BooksContainer
