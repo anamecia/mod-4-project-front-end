d33ee7954b582148e605a2a9e6189b98cba7e746
ana <anameciarobalo@gmail.com>
2020-03-11 16:56:44 +0000

    changes the grid (semantic ui) in the home page component for flexbox

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,19 +9,17 @@ class HomePage extends Component {
 
     render (){
         return(
-            <div className="ui centered middle aligned grid">
-                <div className="five wide column centered middle aligned">
-                    <div className="row">
-                        <h1>Keep track of your readings</h1>
-                        <p><em>"Reading is probably another way of being in a place."</em></p>
-                        <p>― José Saramago, The Double</p>
-                    </div>
-                    <div className="row member-btn-container">
+            <div className="member-container">
+                <div className="slogan-container">
+                    <h1>Keep track of your readings</h1>
+                    <p><em>"Reading is probably another way of being in a place."</em></p>
+                    <p>― José Saramago, The Double</p>
+                    <div className="member-btn-container">
                         <Link className="member-btn" to='/signup'>Sign Up</Link>
                         <Link className="member-btn" to='/signin'>Sign In </Link>
                     </div>
                 </div>
-                <div className="eleven wide column centered middle aligned">
+                <div className="main-image-container">
                     <MainImage />
                 </div>
             </div>
